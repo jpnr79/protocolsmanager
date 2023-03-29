c4ae8a790f601ab4831523dc3d92b0f73fa80920
Mikail <56034854+CanMik@users.noreply.github.com>
2023-03-29 11:58:07 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -6,7 +6,7 @@ function plugin_version_protocolsmanager()
 
         return [
                 'name' => "Protocols manager",
-                'version'        => '1.5.3.4',
+                'version'        => '1.5.3.5',
                 'author'         => 'Mikail',
                 'license'        => 'GPLv3+',
                 'homepage'       => 'https://github.com/CanMik/protocolsmanager',
