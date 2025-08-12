334e3ab80f5090bdbbe607b5a094eae416ce5efe
Mikail <mca@2crsi.com>
2025-08-12 10:28:35 +0200

    clearing setup.php

--- setup.php
+++ setup.php
@@ -1,87 +1,69 @@
 <?php
 
-// Get the name and the version of the plugin - Needed
-function plugin_version_protocolsmanager()
+// Plugin version info
+function plugin_version_protocolsmanager(): array
 {
-
-        return [
-                'name' => "Protocols manager",
-                'version'        => '1.5.5.1',
-                'author'         => 'Mikail',
-                'license'        => 'GPLv3+',
-                'homepage'       => 'https://github.com/CanMik/protocolsmanager',
-                'requirements' => [
-                        'glpi' => [
-                                'min' => '10.0.0',
-                                'max' => '11.0.0'
-                        ],
-                        'php' => [
-                                'min' => '7.4'
-                        ]
-                ]
-        ];
+    return [
+        'name'         => __('Protocols manager', 'protocolsmanager'),
+        'version'      => '1.5.5.2',
+        'author'       => 'Mikail',
+        'license'      => 'GPLv3+',
+        'homepage'     => 'https://github.com/CanMik/protocolsmanager',
+        'requirements' => [
+            'glpi' => [
+                'min' => '10.0.0',
+                'max' => '11.0.0'
+            ],
+            'php'  => [
+                'min' => '7.4'
+            ]
+        ]
+    ];
 }
 
-// Uninstall process for plugin : need to return true if succeeded : may display messages or add to message after redirect
-function plugin_protocolsmanager_check_config()
+// Config check
+function plugin_protocolsmanager_check_config(): bool
 {
-        return true;
+    return true;
 }
 
-// Optional : check prerequisites before install : may print errors or add to message after redirect
-function plugin_protocolsmanager_check_prerequisites()
+// Prerequisites check
+function plugin_protocolsmanager_check_prerequisites(): bool
 {
-        if (version_compare(GLPI_VERSION, '10.0.0', 'lt') || version_compare(GLPI_VERSION, '11.0.0', 'gt')) {
-                if (method_exists('Plugin', 'messageIncompatible')) {
-                        //since GLPI 9.2
-                        Plugin::messageIncompatible('core', '10.0.0', '11.0.0');
-                } else {
-                        echo "This plugin requires GLPI >= 10.0.0 and < 11.0.0";
-                }
-                return false;
+    if (version_compare(GLPI_VERSION, '10.0.0', '<') || version_compare(GLPI_VERSION, '11.0.0', '>')) {
+        if (method_exists('Plugin', 'messageIncompatible')) {
+            Plugin::messageIncompatible('core', '10.0.0', '11.0.0');
+        } else {
+            echo __('This plugin requires GLPI >= 10.0.0 and < 11.0.0', 'protocolsmanager');
         }
-        return true;
+        return false;
+    }
+    return true;
 }
 
-// Init the hooks of the plugins -Needed
-function plugin_init_protocolsmanager()
+// Init plugin hooks
+function plugin_init_protocolsmanager(): void
 {
-        global $PLUGIN_HOOKS;
+    global $PLUGIN_HOOKS;
 
-        $PLUGIN_HOOKS['csrf_compliant']['protocolsmanager'] = true;
+    $PLUGIN_HOOKS['csrf_compliant']['protocolsmanager'] = true;
+    $PLUGIN_HOOKS['add_css']['protocolsmanager']        = 'css/styles.css';
 
+    // Register tabs for supported item types
+    $tabTargets = [
+        'User', 'Printer', 'Peripheral', 'Computer',
+        'Phone', 'Line', 'Monitor'
+    ];
+    foreach ($tabTargets as $target) {
+        Plugin::registerClass('PluginProtocolsmanagerGenerate', ['addtabon' => [$target]]);
+    }
+
+    Plugin::registerClass('PluginProtocolsmanagerProfile', ['addtabon' => ['Profile']]);
+    Plugin::registerClass('PluginProtocolsmanagerConfig',  ['addtabon' => ['Config']]);
+
+    // Config page & menu
+    if (Session::haveRight('config', READ)) {
+        $PLUGIN_HOOKS['menu_toadd']['protocolsmanager'] = ['config' => 'PluginProtocolsmanagerMenu'];
         $PLUGIN_HOOKS['config_page']['protocolsmanager'] = 'front/config.form.php';
-
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('User')));
-
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Printer')));
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Peripheral')));
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Computer')));
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Phone')));
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Line')));
-        Plugin::registerClass('PluginProtocolsmanagerGenerate', array('addtabon' => array('Monitor')));
-
-        Plugin::registerClass('PluginProtocolsmanagerProfile', array('addtabon' => array('Profile')));
-        Plugin::registerClass('PluginProtocolsmanagerConfig', array('addtabon' => array('Config')));
-
-        $PLUGIN_HOOKS['add_css']['protocolsmanager'] = 'css/styles.css';
-        if (Session::haveRight("config", READ)) {
-                $PLUGIN_HOOKS["menu_toadd"]['protocolsmanager'] = ['config' => 'PluginProtocolsmanagerMenu'];
-        }
-
-        if (Session::haveRight('config', READ)) {
-                $PLUGIN_HOOKS['config_page']['protocolsmanager'] = 'front/config.form.php';
-        }
-
-        // if (Session::getLoginUserID()) {
-
-        //         // Config page
-        //         if (Session::haveRight("config", READ) || Session::haveRight("config", UPDATE)) {
-        //                 $PLUGIN_HOOKS['config_page']['protocolsmanager'] = 'front/config.form.php';
-        //         }
-        //         // Menu page
-        //         if (Session::haveRight("config", READ)) {
-        //                 $PLUGIN_HOOKS["menu_toadd"]['protocolsmanager'] = ['config' => 'PluginProtocolsmanagerMenu'];
-        //         }
-        // }
+    }
 }
